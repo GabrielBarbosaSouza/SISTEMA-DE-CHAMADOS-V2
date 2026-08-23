079ed6cee2c6a19a4e098823de76dad4cb94fa91
Gabriel Barbosa Souza <gabrielbarbosasouzabiel@gmail.com>
2026-08-23 17:25:05 -0300

    Mudanças na conexão do aplicativo. Precisei criar uma variável de ambiente para a porta e ssl, assim o app consegue ser hospedado na web.

--- banco/conexao.py
+++ banco/conexao.py
@@ -4,9 +4,11 @@ import mysql.connector
 try:
     db = mysql.connector.connect(
         host=os.environ.get("DB_HOST", "localhost"),
+        port=int(os.environ.get("DB_PORT", "3306")),
         user=os.environ.get("DB_USER", "root"),
         password=os.environ.get("DB_PASSWORD", ""),
         database=os.environ.get("DB_NAME", "script"),
+        ssl_disabled=os.environ.get("DB_SSL_DISABLED", "true").lower() == "true",
     )
 
 except mysql.connector.Error as erro:
