345918f15efeec3a3d6fb71e2a6487c4ed173b29
Gabriel Barbosa Souza <gabrielbarbosasouzabiel@gmail.com>
2026-09-02 10:38:27 -0300

    Adição das colunas data_abertura e data_fechamento no Jinja em chamados.html e na rota \chamados

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def listar_chamados():
     
     cursor.execute(
         """
-        SELECT id, titulo, descricao, categoria, prioridade, status
+        SELECT id, titulo, descricao, categoria, prioridade, status, data_abertura, data_fechamento
         FROM chamados
         ORDER BY id DESC
         """
